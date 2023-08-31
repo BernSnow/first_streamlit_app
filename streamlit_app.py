520d2a6bd249b17cc3305453ed63a27f4a31eb48
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 15:41:21 -0600

    Update streamlit_app.py
    Make a dataframe and display it based on the json data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,9 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+
+#Makes the json format to a nice pandas dataframe.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#Shows the dataframe on screen.
+streamlit.dataframe(fruityvice_normalized)
