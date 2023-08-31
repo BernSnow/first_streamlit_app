119ee2408a1162f8aff05674bc32776ed277fb8e
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 15:46:19 -0600

    Update streamlit_app.py
    Added Text Entry Box to make more dynamic to search the API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,10 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 #Makes the json format to a nice pandas dataframe.
