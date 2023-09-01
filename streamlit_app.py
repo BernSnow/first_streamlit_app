10308e72f0d3459a44763ec084fa5f6ffda562ba
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 21:39:33 -0600

    Update streamlit_app.py
    Forgot the error message on the if of the try block.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-        streamlit.write('The user entered ', fruit_choice)
+        #streamlit.write('The user entered ', fruit_choice)
+        streamlit.error('Please select a fruit to get information.")
     else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
