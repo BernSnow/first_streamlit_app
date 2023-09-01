ef76477bf9be919dad197b276407f750f3eea188
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 21:40:26 -0600

    Update streamlit_app.py
    fix string typo on error of the if block on try.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         #streamlit.write('The user entered ', fruit_choice)
-        streamlit.error('Please select a fruit to get information.")
+        streamlit.error("Please select a fruit to get information.")
     else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
