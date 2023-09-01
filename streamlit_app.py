03ac457e9c4f389e9449d5cc911d26de5735b31f
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 21:27:29 -0600

    Update streamlit_app.py
    Organize the app (kinda did that in the first place, but the tutorial suggest to do it).

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom\'s New Healthy Diner")
 streamlit.header('Breakfast Favorites')
