1d2b70039c8b5b4fb0207c21e8df57e8bd682e9f
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 21:25:28 -0600

    Update streamlit_app.py
    Add a Stop command to focus the attention

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Shows the dataframe on screen.
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 #Snowflake connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
