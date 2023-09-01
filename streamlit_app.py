675a78e3e86c1890d285c4978ce2c2c1c67dc3d4
BernSnow <143562314+BernSnow@users.noreply.github.com>
2023-08-31 23:57:30 -0600

    Update streamlit_app.py
    Commented a stop that omit the flow of the program.

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if streamlit.button('Get Fruit Load List'):
     #streamlit.dataframe(my_data_row)
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
